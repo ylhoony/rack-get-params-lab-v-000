0cc31fa803ec78d1c7c414d971047a7679d25c80
Hoon Lee <yl.hoony@gmail.com>
2018-01-02 03:15:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,8 +22,8 @@ class Application
           resp.write "#{item}\n"
         end
       end
-    elsif req.path.match(/add/)
-      
+    # elsif req.path.match(/add/)
+
     else
       resp.write "Path Not Found"
     end
