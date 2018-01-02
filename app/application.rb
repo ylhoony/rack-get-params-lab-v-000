752eedb4ca2506373ad73ec88a512beab8e48f6d
Hoon Lee <yl.hoony@gmail.com>
2018-01-02 03:16:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.empty?
-        resp.wirite "Your cart is empty"
+        resp.write "Your cart is empty"
       else
         @@cart.each do |item|
           resp.write "#{item}\n"
